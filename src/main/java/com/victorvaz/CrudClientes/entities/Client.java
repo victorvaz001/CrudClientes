273d2e7054bbf09a3a359597015476a37f1d8de5
victorvaz001 <victorvaz001@gmail.com>
2023-12-12 16:25:36 -0300

    Mapeamento ORM Client

--- src/main/java/com/victorvaz/CrudClientes/entities/Client.java
+++ src/main/java/com/victorvaz/CrudClientes/entities/Client.java
@@ -1,11 +1,17 @@
 package com.victorvaz.CrudClientes.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_client")
 public class Client implements Serializable {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
