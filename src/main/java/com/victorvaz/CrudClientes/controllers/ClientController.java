a40d8b24920062ccd498392779c02046b1a4083e
victorvaz001 <victorvaz001@gmail.com>
2023-12-15 11:37:06 -0300

    Api Bean Validation parte 2 - Exceções personalizadas, novo metodo no hendler

--- src/main/java/com/victorvaz/CrudClientes/controllers/ClientController.java
+++ src/main/java/com/victorvaz/CrudClientes/controllers/ClientController.java
@@ -2,11 +2,15 @@ package com.victorvaz.CrudClientes.controllers;
 
 import com.victorvaz.CrudClientes.dto.ClientDTO;
 import com.victorvaz.CrudClientes.services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -28,9 +32,12 @@ public class ClientController {
     }
 
     @PostMapping
-    public ClientDTO insert(@RequestBody ClientDTO dto) {
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
+        dto = clientService.insert(dto);
 
-        return clientService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
     @PutMapping(value = "/{id}")
--- src/main/java/com/victorvaz/CrudClientes/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/victorvaz/CrudClientes/controllers/handlers/ControllerExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.victorvaz.CrudClientes.controllers.handlers;
 
 import com.victorvaz.CrudClientes.dto.CustomError;
+import com.victorvaz.CrudClientes.dto.ValidationError;
 import com.victorvaz.CrudClientes.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,4 +21,17 @@ public class ControllerExceptionHandler {
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(),request.getRequestURI() );
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {  //Exceções geradas conforme as annotation do ProductDTO
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
